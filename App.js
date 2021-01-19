cf28428f13c401d875ff7730b2949afd7585be3a
Johan <johanlindstroom@icloud.com>
2021-01-19 01:34:19 +0100

    Array was formatted wrong, Everything displays fine now

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View, LogBox} from 'react-native';
+import { StyleSheet, Text, View, FlatList, Image} from 'react-native';
 import Home from './screens/Home.js'
-
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
       <Home />
     </View>
   );
@@ -13,8 +12,6 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
--- components/List.js
+++ components/List.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import { SafeAreaView, Text, View, FlatList, Image} from 'react-native';
+import { SafeAreaView, Text, View, FlatList, Image, StyleSheet, TouchableOpacity} from 'react-native';
 
 const plantImg = require('../assets/favicon.png')
 
-const DummyPlantData = () => {[
+const DummyPlantData = [
     {id:1, img: plantImg, title: 'Plant Title', reminderTime: 'Water every * days', timeLeft: 'Time left'},
     {id:2, img: plantImg, title: 'Plant Title', reminderTime: 'Water every * days', timeLeft: 'Time left'},
     {id:3, img: plantImg, title: 'Plant Title', reminderTime: 'Water every * days', timeLeft: 'Time left'},
-]}
+]
  
-export default function List() {
+const List = () => {
     return (
-        <View style={{height:'100%'}}>
+        <SafeAreaView style={{height:'100%',}}>
           <FlatList
             data={DummyPlantData}
             renderItem={({ item }) => {
@@ -20,7 +20,7 @@ export default function List() {
                   <View style={{ backgroundColor: 'gray', marginTop:10 ,width:'100%', height:120, flexDirection:'row', alignItems:'center'}}>
                     
                     <View style={{flex:0.5, backgroundColor:'orange', marginLeft:20}}>
-                      <Image source={item.plantImg} style={{ height: 100, width: 100 }} />
+                      <Image source={item.img} style={{ height: 100, width: 100 }} />
                     </View>
                     
                     <View style={{flex:1, backgroundColor:'red', height:100, marginLeft:10, marginRight:10, justifyContent:'center'}}>
@@ -40,6 +40,16 @@ export default function List() {
               );
             }}
           />
-        </View>
+        </SafeAreaView>
       )
 }
+
+const styles = StyleSheet.create({
+    container: {
+    flex:1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    },
+  });
+
+export default List;
